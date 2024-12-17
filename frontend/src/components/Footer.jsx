194cd3318b5f9e51109d22adddc27307f9ad985f
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-17 21:15:19 +0100

    Auto commit at 12/17/2024, 9:15:19 PM

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,15 +4,15 @@ import { assets } from '../assets/assets'
 const Footer = () => {
     return (
         <div className='md:mx-10'>
-            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14">
+            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
                 <div className="">
-                    <img src={assets.logo} alt="Logo" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque obcaecati ab autem in sint mollitia quae commodi aut explicabo dolor minima repudiandae molestias, alias facilis aspernatur molestiae. Fugiat, omnis doloribus.</p>
+                    <img className='mb-5 w-40' src={assets.logo} alt="Logo" />
+                    <p className='w-full md:w-2/3 text-gray-500 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque obcaecati ab autem in sint mollitia quae commodi aut explicabo dolor minima repudiandae molestias, alias facilis aspernatur molestiae. Fugiat, omnis doloribus.</p>
                 </div>
 
                 <div className="">
-                    <p>COMPANY</p>
-                    <ul>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
                         <li>Home</li>
                         <li>About us</li>
                         <li>Contact us</li>
