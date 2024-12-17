9af3015fd574ac4d873bc95145ea00b521848740
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-17 21:30:19 +0100

    Auto commit at 12/17/2024, 9:30:19 PM

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,14 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 
     const {speciality} = useParams()
-    const [filterDoc,]
+    const [filterDoc, setFilterDoc] = useState([])
 
     // console.log(speciality)
     const {doctors} = useContext(AppContext)
+
+    const applyFilter = () => {
+        if (speciality) {
+            setFilterDoc(doctors.filter(doc => doc.speciality === speciality))
+        } else {
+            setFilterDoc(doctors)
+        }
+    }
+
+    useEffect(() => {
+
+    }, [])
 
     return (
         <div>
@@ -24,7 +36,18 @@ const Doctors = () => {
                 </div>
                 <div className="">
                     {
-
+                        filterDoc.map((item, index) => (
+                            <div onClick={() => navigate(`/appointment/${item._id}`)} key={index} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+                                <img className='bg-blue-50' src={item.image} alt="Doctor's Image" />
+                                <div className="p-4">
+                                    <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                                    </div>
+                                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                                    <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                                </div>
+                            </div>
+                        ))
                     }
                 </div>
             </div>
