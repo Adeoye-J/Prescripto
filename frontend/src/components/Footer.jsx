4e019cd1e911e956ed732202e992271e05dc5a18
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-17 21:12:02 +0100

    Auto commit at 12/17/2024, 9:12:02 PM

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
     return (
         <div className='md:mx-10'>
-            <div className="flex flex-col sm:grid grid-cols- gap-14">
+            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14">
                 <div className="">
                     <img src={assets.logo} alt="Logo" />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque obcaecati ab autem in sint mollitia quae commodi aut explicabo dolor minima repudiandae molestias, alias facilis aspernatur molestiae. Fugiat, omnis doloribus.</p>
