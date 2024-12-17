e57c4d7b24690878b6f6b9d99beb75cc887f468b
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-17 21:25:19 +0100

    Auto commit at 12/17/2024, 9:25:19 PM

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 
     const {speciality} = useParams()
+    const [filterDoc,]
 
-    console.log(speciality)
+    // console.log(speciality)
+    const {doctors} = useContext(AppContext)
 
     return (
-        <div>Doctors</div>
+        <div>
+            <p>Browse through the doctors specialist.</p>
+            <div className="">
+                <div className="">
+                    <p>General Physician</p>
+                    <p>Gynecologist</p>
+                    <p>Dermatologist</p>
+                    <p>Pediatricians</p>
+                    <p>Neurologist</p>
+                    <p>Gastroenterologist</p>
+                </div>
+                <div className="">
+                    {
+
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
