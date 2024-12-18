e34ce0e8cd850385c2b71eef4ed8ebbf04e452ce
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-18 12:25:04 +0100

    Auto commit at 12/18/2024, 12:25:03 PM

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -32,16 +32,20 @@ const Appointment = () => {
                 <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
                     <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
                         {docInfo.name} 
-                        <img src={assets.verified_icon} alt="Verified Icon" />
+                        <img className='w-5' src={assets.verified_icon} alt="Verified Icon" />
                     </p>
-                    <div className="">
+                    <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
                         <p>{docInfo.degree} - {docInfo.speciality}</p>
-                        <button>{docInfo.experience}</button>
+                        <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
                     </div>
                     <div className="">
-                        <p>About <img src={assets.info_icon} alt="Info Icon" /></p>
-                        <p>{docInfo.about}</p>
+                        <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+                            About 
+                            <img src={assets.info_icon} alt="Info Icon" />
+                        </p>
+                        <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
                     </div>
+                    <p>Appointment fee:</p>
                 </div>
             </div>
         </div>
