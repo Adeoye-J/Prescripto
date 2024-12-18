c9f7e7e6a4198c51e1245ceb19ca5276e521601f
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-18 14:40:02 +0100

    Auto commit at 12/18/2024, 2:40:02 PM

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RelatedDoctors = ({docId, speciality}) => {
+
+    const {doctors} = use
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RelatedDoctors
