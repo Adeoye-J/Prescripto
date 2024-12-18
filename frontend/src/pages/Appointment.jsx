bb881a2fa907cf8aff0ddeadb773ee60bfb04a8c
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-18 12:20:04 +0100

    Auto commit at 12/18/2024, 12:20:04 PM

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -24,19 +24,23 @@ const Appointment = () => {
     
     return docInfo && (
         <div>
-            <div className="">
+            <div className="flex flex-col sm:flex-row gap-4">
                 <div className="">
-                    <img src={docInfo.image} alt="" />
+                    <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
                 </div>
 
-                <div className="">
-                    <p>{docInfo.name} <img src={assets.verified_icon} alt="Verified Icon" /></p>
+                <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+                    <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+                        {docInfo.name} 
+                        <img src={assets.verified_icon} alt="Verified Icon" />
+                    </p>
                     <div className="">
                         <p>{docInfo.degree} - {docInfo.speciality}</p>
                         <button>{docInfo.experience}</button>
                     </div>
                     <div className="">
-                        <p>About <img src={assets.info_icon} alt="Info Io" /></p>
+                        <p>About <img src={assets.info_icon} alt="Info Icon" /></p>
+                        <p>{docInfo.about}</p>
                     </div>
                 </div>
             </div>
