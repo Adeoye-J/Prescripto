98091bff9d6113c3ec8a3661e539f3a537bdbe6f
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-18 08:00:03 +0100

    Auto commit at 12/18/2024, 8:00:03 AM

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -24,6 +24,7 @@ const Appointment = () => {
         <div>
             <p>Test content</p>
             <p>{docInfo?.name}</p>
+            <img src={docInfo?.image} alt="" />
         </div>
     )
 }
