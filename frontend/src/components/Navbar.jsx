27b162e026e3e9969aeb194552569ee8bfbf917b
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-20 09:00:02 +0100

    Auto commit at 12/20/2024, 9:00:02 AM

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,11 +54,11 @@ const Navbar = () => {
                         <img className='w-36' src={assets.logo} alt="Logo" />
                         <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="Cross Icon" />
                     </div>
-                    <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-                        <NavLink>Home</NavLink>
-                        <NavLink>All Doctors</NavLink>
-                        <NavLink>About</NavLink>
-                        <NavLink>Contact</NavLink>
+                    <ul className='flex flex-col items-center gap-5 mt-5 px-5 text-lg font-medium uppercase'>
+                        <NavLink onClick={() => setShowMenu(false)} to={"/"}><p className="px-4 py-2 rounded inline-block">Home</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to={"/doctors"}><p className="px-4 py-2 rounded inline-block">All Doctors</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to={"/about"}><p className="px-4 py-2 rounded inline-block">About</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to={"/contact"}><p className="px-4 py-2 rounded inline-block">Contact</p></NavLink>
                     </ul>
                 </div>
             </div>
