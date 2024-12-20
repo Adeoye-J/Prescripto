acc6e1fe7f7982c01ca6399ca30727dba8753631
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-20 08:50:02 +0100

    Auto commit at 12/20/2024, 8:50:02 AM

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,6 +47,20 @@ const Navbar = () => {
                     : <button onClick={() => navigate("/login")} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
                 }
                 
+                <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                {/* Mobile Menu */}
+                <div className={`${showMenu ? "fixed w-full" : "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                    <div className="flex items-center justify-between px-5 py-6">
+                        <img className='w-36' src={assets.logo} alt="Logo" />
+                        <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="Cross Icon" />
+                    </div>
+                    <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                        <NavLink>Home</NavLink>
+                        <NavLink>All Doctors</NavLink>
+                        <NavLink>About</NavLink>
+                        <NavLink>Contact</NavLink>
+                    </ul>
+                </div>
             </div>
         </div>
     )
