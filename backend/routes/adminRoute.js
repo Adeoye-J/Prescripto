ab4092eb8c62f44caec4abda47013044f0f6cc48
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-22 00:25:02 +0100

    Auto commit at 12/22/2024, 12:25:00 AM

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -5,6 +5,6 @@ import upload from "../middlewares/multer.js"
 const adminRouter = express.Router()
 
 adminRouter.post("/add-doctor", upload.single("image"), addDoctor)
-adminRouter.post("/login", upload.single("image"), addDoctor)
+adminRouter.post("/login", adminLogin)
 
 export default adminRouter
