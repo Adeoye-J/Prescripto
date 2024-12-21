819f94a2e6de442133e500e746cfaf12e7c3da48
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-21 23:40:03 +0100

    Auto commit at 12/21/2024, 11:40:03 PM

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -7,7 +7,12 @@ const addDoctor = async (req, res) => {
         const {name, email, password, speciality, degree, experience, about, fees, address} = req.body
         const imageFile = req.imageFile
 
-        console.log({name, email, password, speciality, degree, experience, about, fees, address}, imageFile)
+        // Checking for all data to add doctor
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+
+        }
+
+        // console.log({name, email, password, speciality, degree, experience, about, fees, address}, imageFile)
     } catch (error) {
         
     }
