e900cc581349a07724d79c1e69510ca1a192ff72
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-22 20:40:03 +0100

    Auto commit at 12/22/2024, 8:40:02 PM

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import {assets} from "../assets/assets"
+import { useState } from 'react'
 
 const Login = () => {
 
-    const 
+    const [state, setState] = useState("Admin")
 
     return (
-        <div>
-            
-        </div>
+        <form className="min-h-[80vh] flex items-center">
+            <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg'>
+                <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
+                <div className="">
+                    <p>Email:</p>
+                    <input type="email" required />
+                </div>
+                <div className="">
+                    <p>Password:</p>
+                    <input type="password" required />
+                </div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
 
