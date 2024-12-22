4cfc524960a866ab0524431f0ffc25d1769f1ea8
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-22 20:30:03 +0100

    Auto commit at 12/22/2024, 8:30:02 PM

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,9 +3,18 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import AdminContextProvider from './context/AdminContext.jsx'
+import DoctorContextProvider from './context/DoctorContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <BrowserRouter>
+        <AdminContextProvider>
+            <DoctorContextProvider>
+                <AppContextProvider>
+                    <App />
+                </AppContextProvider>
+            </DoctorContextProvider>
+        </AdminContextProvider>
+    </BrowserRouter>,
 )
