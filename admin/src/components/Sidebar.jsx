8b6e8bde5dfe6573f73a0c1d5b88efba9cf62a81
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-23 20:00:05 +0100

    Auto commit at 12/23/2024, 8:00:03 PM

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -12,19 +12,19 @@ const Sidebar = () => {
         <div className='min-h-screen bg-white border-r'>
             {
                 aToken && <ul className='text-[#515151] mt-5'>
-                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg" : ""}`} to={"/admin-dashboard"}>
+                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive && "bg-[#F2F3FF] border-r-4 border-primary"}`} to={"/admin-dashboard"}>
                         <img src={assets.home_icon} alt="Home Icon" />
                         <p>Dashboard</p>
                     </NavLink>
-                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg" : ""}`} to={"/all-appointments"}>
+                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive && "bg-[#F2F3FF] border-r-4 border-primary"}`} to={"/all-appointments"}>
                         <img src={assets.appointment_icon} alt="Home Icon" />
                         <p>Appointment</p>
                     </NavLink>
-                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg" : ""}`} to={"/add-doctor"}>
+                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive && "bg-[#F2F3FF] border-r-4 border-primary"}`} to={"/add-doctor"}>
                         <img src={assets.add_icon} alt="Home Icon" />
                         <p>Add Doctor</p>
                     </NavLink>
-                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg" : ""}`} to={"/doctor-list"}>
+                    <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive && "bg-[#F2F3FF] border-r-4 border-primary"}`} to={"/doctor-list"}>
                         <img src={assets.people_icon} alt="Home Icon" />
                         <p>Doctors List</p>
                     </NavLink>
