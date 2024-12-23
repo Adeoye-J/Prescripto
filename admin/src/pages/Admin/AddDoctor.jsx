06ed84e0aa5d3bb3449fedf550f3c2445a480107
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-23 20:30:03 +0100

    Auto commit at 12/23/2024, 8:30:02 PM

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
     return (
-        <div>AddDoctor</div>
+        <form>
+            <p>Add Doctor</p>
+
+            <div className="">
+                <div className="">
+                    <label htmlFor="">
+                        <img src={assets.upload_area} alt="Upload Area" />
+                    </label>
+                </div>
+            </div>
+        </form>
     )
 }
 
