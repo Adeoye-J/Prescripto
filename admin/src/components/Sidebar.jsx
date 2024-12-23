f0e3ed1860b48c6413a6743b3fbd92089248a392
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-23 19:40:03 +0100

    Auto commit at 12/23/2024, 7:40:02 PM

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import {NavLink} from "react-router-dom"
+import { assets } from '../assets/assets'
 
 const Sidebar = () => {
+
+    const {aToken} = useContext(AdminContext)
+
     return (
         <div>
-            jkj
+            {
+                aToken && <ul>
+                    <NavLink>
+                        <img src={assets.home_icon} alt="Home Icon" />
+                        <p>Dashboard</p>
+                    </NavLink>
+                    <NavLink>
+                        <img src={assets.appointment_icon} alt="Home Icon" />
+                        <p>Dashboard</p>
+                    </NavLink>
+                    <NavLink>
+                        <img src={assets.home_icon} alt="Home Icon" />
+                        <p>Dashboard</p>
+                    </NavLink>
+                    <NavLink>
+                        <img src={assets.home_icon} alt="Home Icon" />
+                        <p>Dashboard</p>
+                    </NavLink>
+                </ul>
+            }
         </div>
     )
 }
