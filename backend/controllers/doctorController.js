3876292373ce4d016d59fb2c7e6327ce8a4e1cb7
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-24 09:40:04 +0100

    Auto commit at 12/24/2024, 9:40:03 AM

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -2,13 +2,21 @@
 
 // Change Doctor Availability
 
+import doctorModel from "../models/doctorModel.js"
+
 const changeAvailability = async (req, res) => {
     try {
         
+        const {docId} = req.body
+
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId, {available: !docData.available})
+        res.json({success: true, message: "Availability Status Changed"})
+
     } catch (error) {
         console.log(error)
         res.json({success: false, message: error.message})
     }
 }
 
-export {}
+export {changeAvailability}
