dd8ce718a1a6de7d0a8bf1f961a711d752887516
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-24 09:50:04 +0100

    Auto commit at 12/24/2024, 9:50:03 AM

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,9 +1,31 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+// import { doctors } from "../assets/assets";
+import axios from "axios"
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [doctors, setDoctors] = useState([])
+
+    const getDoctorsData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/doctor/list")
+            if (data.success) {
+                setDoctors(data.doctors)
+            }
+            
+        } catch (error) {
+            toast
+            console.log(error);
+            
+        }
+    }
+
+    useEffect(() => {
+        getDoctorsData()
+    }, [])
 
 
     const currencySymbol = "$"
