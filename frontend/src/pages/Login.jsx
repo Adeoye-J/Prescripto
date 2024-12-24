7cef00f00aa64619801c2047f7777002462d4925
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-24 14:20:05 +0100

    Auto commit at 12/24/2024, 2:20:03 PM

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
@@ -43,6 +43,12 @@ const Login = () => {
         }
     }
 
+    useEffect(() => {
+        if (token) {
+            navigate("/")
+        }
+    }, [token])
+
     return (
         <form onSubmit={onSubmitHandler} action="" className='min-h-[80vh] flex items-center'>
             <div className="flex flex-col gap-3 m-auto p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
@@ -52,16 +58,16 @@ const Login = () => {
                     state === "Sign Up" &&
                     <div className="w-full">
                         <p>Full Name</p>
-                        <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" value={name} onChange={() => setName(e.target.value)} required/>
+                        <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
                     </div>
                 }
                 <div className="w-full">
                     <p>Email</p>
-                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" value={email} onChange={() => setEmail(e.target.value)} required/>
+                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 </div>
                 <div className="w-full">
                     <p>Password</p>
-                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" value={password} onChange={() => setPassword(e.target.value)} required/>
+                    <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                 </div>
                 <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === "Sign Up" ? "Create Account" : "Login"}</button>
                 {
