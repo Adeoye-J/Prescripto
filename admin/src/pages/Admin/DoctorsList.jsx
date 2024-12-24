ddfab5b092f4980db2c5f85a658823cc6302145c
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-24 09:20:04 +0100

    Auto commit at 12/24/2024, 9:20:03 AM

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -14,19 +14,20 @@ const DoctorsList = () => {
     }, [aToken])
 
     return (
-        <div>
-            <h1>All Doctors</h1>
+        <div className='m-5 max-h-[90vh] overflow-y-scroll'>
+            <h1 className='text-lg font-medium'>All Doctors</h1>
 
-            <div className="">
+            <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
                 {
                     doctors.map((item, index) => (
-                        <div className="">
+                        <div key={index} className="border border-indigo-200 rounded-xl max">
                             <img src={item.image} alt="Doctors Image" />
                             <div className="">
                                 <p>{item.name}</p>
                                 <p>{item.speciality}</p>
                                 <div className="">
                                     <input type="checkbox" checked={item.available} />
+                                    <p>{item.available ? "Available" : "Not Available"}</p>
                                 </div>
                             </div>
                         </div>
