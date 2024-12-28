ff102a354bff204c9fdf7464f681572582e89e04
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-28 16:55:04 +0100

    Auto commit at 12/28/2024, 4:55:03 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,8 @@ const App = () => {
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/my-profile' element={<MyProfile />} />
                 <Route path='/my-appointments' element={<MyAppointments />} />
+                <Route path='/my-appointments/success' element={<MyAppointments />} />
+                <Route path='/my-appointments/cancelled' element={<MyAppointments />} />
                 <Route path='/appointment/:docId' element={<Appointment />} />
             </Routes>
             <Footer />
