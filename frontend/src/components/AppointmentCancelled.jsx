96f4f08d00f05217dc76cf3ee7343e3a38a43527
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-29 00:55:03 +0100

    Auto commit at 12/29/2024, 12:55:02 AM

--- frontend/src/components/AppointmentCancelled.jsx
+++ frontend/src/components/AppointmentCancelled.jsx
@@ -8,8 +8,8 @@ const AppointmentCancelled = () => {
         <div className="fixed top-0 bottom-0 left-0 right-0 bg-red-400 flex items-center justify-center">
             <div className="bg-white p-10 rounded-lg text-center shadow-md">
                 <h2 className='text-red-400 font-bold pb-6'>Payment Cancelled!</h2>
-                <p className='pb-6'>Your transaction ID is: {sessionId}</p>
-                <button onClick={navigate("/my-appointments")} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Close</button>
+                {/* <p className='pb-6'>Your transaction ID is: {sessionId}</p> */}
+                <button onClick={() => navigate("/my-appointments")} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Close</button>
             </div>
         </div>
     )
