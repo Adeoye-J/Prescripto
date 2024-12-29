7c32a06695d4db2025b0280ecde07a6578e23f73
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-29 22:05:03 +0100

    Auto commit at 12/29/2024, 10:05:03 PM

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -17,7 +17,7 @@ const AllAppointments = () => {
         <div className='w-full max-w-6xl m-5'>
             <p className='mb-3 text-lg font-medium'>All Appointments</p>
             <div className="bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll">
-                <div className="hidden">
+                <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
                     <p>#</p>
                     <p>Patient</p>
                     <p>Age</p>
@@ -26,6 +26,18 @@ const AllAppointments = () => {
                     <p>Fees</p>
                     <p>Actions</p>
                 </div>
+
+                {
+                    appointments.map((item, index) => (
+                        <div key={index} className="">
+                            <p>{index + 1}</p>
+                            <div className="">
+                                <img src={item.userData.image} alt="User Image" />
+                                <p>{item.user}</p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
