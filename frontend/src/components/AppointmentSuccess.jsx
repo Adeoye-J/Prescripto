917cabc35291c2720eac6a6997cd15b8a4437f2e
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-29 01:00:05 +0100

    Auto commit at 12/29/2024, 1:00:03 AM

--- frontend/src/components/AppointmentSuccess.jsx
+++ frontend/src/components/AppointmentSuccess.jsx
@@ -6,13 +6,13 @@ const AppointmentSuccess = () => {
 
     return (
         <div className="fixed top-0 bottom-0 left-0 right-0 bg-primary flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg text-center shadow-md">
-                <h2 className='text-blue-400 font-bold'>Payment Successful!</h2>
+            <div className="bg-white p-10 rounded-lg text-center shadow-md">
+                <h2 className='text-blue-400 font-bold pb-6'>Payment Successful!</h2>
                 {/* <p>Your transaction ID is: {sessionId}</p> */}
                 <button onClick={() => navigate("/my-appointments")} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Close</button>
             </div>
         </div>
     )
 }
-
+    
 export default AppointmentSuccess
