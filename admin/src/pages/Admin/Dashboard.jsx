90d9c117eb079d69e0be5467aa0cf4b27c93aa69
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-30 00:35:03 +0100

    Auto commit at 12/30/2024, 12:35:03 AM

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { useContext } from 'react'
 import { AdminContext } from '../../context/AdminContext'
+import { useEffect } from 'react'
+import { assets } from '../../assets/assets'
 
 const Dashboard = () => {
 
-    const {getDashboardData, dashboardData} = useContext(AdminContext)
+    const {getDashboardData, dashboardData, aToken, cancelAppointment} = useContext(AdminContext)
 
-    return (
-        <div>Dashboard</div>
+    useEffect(() => {
+        if (aToken) {
+            getDashboardData()
+        }
+    }, [aToken])
+
+    return dashboardData && (
+        <div className='m-5'>
+            <div className="">
+                <div className="">
+                    <img src={assets.doctor_icon} alt="Doctor Icon" />
+                    <div className="">
+                        <p>{dashboardData.doctors}</p>
+                        <p>Doctors</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
