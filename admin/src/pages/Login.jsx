306c9e451a2a66c96a73ed91430c4f3fa66115fc
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-31 06:15:03 +0100

    Auto commit at 12/31/2024, 6:15:02 AM

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import {assets} from "../assets/assets"
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 
 const Login = () => {
 
@@ -11,6 +12,7 @@ const Login = () => {
     const [password, setPassword] = useState("")
 
     const {setAToken, backendUrl} = useContext(AdminContext)
+    const {setDToken, backendUrl} = useContext(DoctorContext)
 
     const onSubmitHandler = async (event) => {
         event.preventDefault()
@@ -26,7 +28,7 @@ const Login = () => {
                     toast.error(data.message)
                 }
             } else {
-
+                const 
             }
         } catch (error) {
             console.log(error);
