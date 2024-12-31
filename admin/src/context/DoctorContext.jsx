e5ca37ce8a216577d14a089ee54cfdf23bb4f959
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-12-31 06:40:03 +0100

    Auto commit at 12/31/2024, 6:40:02 AM

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react";
+import axios from "axios"
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext()
 
@@ -7,6 +9,21 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [dToken, setDToken] = useState(localStorage.getItem("dToken") || "")
+    const [docAppointments, setDocAppointments] = useState([])
+
+    const getDocAppointments = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/doctor/appointments", {headers: {dToken}})
+            if (data.success) {
+                setDocAppointments(data.appointments.reverse())
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         dToken,
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { doctorList, doctorLogin } from "../controllers/doctorController.js"
+import { doctorAppointments, doctorList, doctorLogin } from "../controllers/doctorController.js"
+import authDoctor from "../middlewares/authDoctor.js"
 
 const doctorRouter = express.Router()
 
 doctorRouter.get("/list", doctorList)
 
 doctorRouter.post("/login", doctorLogin)
+
+doctorRouter.get("/appointments", authDoctor,doctorAppointments)
 
 export default doctorRouter
